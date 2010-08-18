17857d5f5f3bd104b384200e522fed1e60ca59c3
Mitch Garnaat <mitch@cloudright.com>
2010-08-18 01:37:18 -0400

    Adding setup.py file.

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+# Copyright (c) 2010 Mitch Garnaat http://garnaat.org/
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+from distutils.core import setup
+
+setup(name = "nasbox",
+      version = '0.1a',
+      description = "S3 NAS",
+      long_description="A NAS device that backs up to S3",
+      author = "Mitch Garnaat",
+      author_email = "mitch@garnaat.com",
+      scripts = ["bin/nas_index"],
+      url = "put URL here",
+      packages = [ 'nasbox'],
+      license = 'MIT',
+      platforms = 'Posix; MacOS X; Windows',
+      classifiers = [ 'Development Status :: 2 - Pre-Alpha',
+                      'Intended Audience :: Developers',
+                      'License :: OSI Approved :: MIT License',
+                      'Operating System :: OS Independent',
+                      'Topic :: Internet',
+                      ],
+      )
