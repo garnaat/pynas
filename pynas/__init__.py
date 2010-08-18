35a8548df4ac79d90a5af1e81751cc32485b081f
Mitch Garnaat <mitch@cloudright.com>
2010-08-18 01:49:18 -0400

    Adding copyright notices.

--- pynas/__init__.py
+++ pynas/__init__.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2010 Mitch Garnaat http://garnaat.org/
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 import hashlib
 import os
 import datetime
@@ -26,7 +47,7 @@ class NASIndexer(object):
     A 38-digit hexadecimal string.  We then use the GIT trick of
     using the first two digits of this hash as a directory name
     and the remaining digits as the name of a file within that
-    directory.  So, if our NAS index is stored in ~/.nasbox we
+    directory.  So, if our NAS index is stored in ~/.pynas we
     would see 256 directories within that main index directory
     with each directory named as a two-digit hexadecimal number:
 
@@ -97,7 +118,7 @@ class NASIndexer(object):
     """
 
     def __init__(self, path, index_path,
-                 ignore_dirs=['.svn', '.git', '.nasbox']):
+                 ignore_dirs=['.svn', '.git', '.pynas']):
         """
         The constructor for the class.
 
